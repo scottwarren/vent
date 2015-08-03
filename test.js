ec0daf88a60b39aba8e5cd29d7f04d51803cc2db
Scott Warren <scott_warren@flightcentre.com>
2015-08-03 17:45:35 +1000

    Updated vent tests to confirm if triggered

--- test.js
+++ test.js
@@ -6,11 +6,11 @@ assert.equal(vent.hasEvent('test'), false, 'check that no events are bound');
 assert.throws(function() { vent.trigger('test'); }, Error, 'check that an error is thrown');
 
 var exampleEvent = vent.on('test', function() {
-  // do stuff
+  return 'I ran!'
 });
 
 // need some way to check if an event is being run
-assert.equal(vent.trigger('test'), undefined, 'check that the bound event is ran');
+assert.equal(vent.trigger('test'), 'I ran!', 'check that the bound event is ran');
 assert.equal(vent.hasEvent('test'), true, 'check that an event is bound for test');
 
 vent.off(exampleEvent);
