30b231bf4b11e45c65ac1b067a154a4ed2955f60
Scott Warren <scott_warren@flightcentre.com>
2015-08-03 17:58:51 +1000

    Updated check for bound event

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
 var assert = require('assert'),
-  vent = require('./vent.js');
+  vent = require('./vent.js'),
+  // this checks if an event is being run
+  hasMyTestRun = false;
 
 assert.equal(vent.hasEvent('test'), false, 'check that no events are bound');
 
 assert.throws(function() { vent.trigger('test'); }, Error, 'check that an error is thrown');
 
+
 var exampleEvent = vent.on('test', function() {
-  return 'I ran!'
+  hasMyTestRun = true;
 });
 
-// need some way to check if an event is being run
-assert.equal(vent.trigger('test'), 'I ran!', 'check that the bound event is ran');
+// run this manually so we can see if the hasMyTestRun value has changed
+vent.trigger('test')
+assert.equal(hasMyTestRun, true, 'check that the bound event is ran');
 assert.equal(vent.hasEvent('test'), true, 'check that an event is bound for test');
 
 vent.off(exampleEvent);
